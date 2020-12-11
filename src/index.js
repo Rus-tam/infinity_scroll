0ea0e77cb2f59d179e03703066d500e80976a74d
Rustam <ahmetov.rustam2011@gmail.com>
2020-12-11 23:47:16 +0500

    infinity_scroll

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 const imageContainer = document.getElementById('image-container')
 const loader = document.getElementById('loader')
 
+let ready = false
+let imagesLoaded = 0
+let totalImages = 0
 let photosArray = []
+
+// Unsplash API
+const count = 30
+const apiKey = 'FKqHOTgeujR0boBSA4NHJTez2AlbMP8d36AF2-71rCw'
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
+
+//Check if all images were loaded
+const imageLoaded = () => {
+    ready = false
+    imagesLoaded++
+    if (imagesLoaded === totalImages) {
+        ready = true
+        loader.hidden = true
+    }
+}
+
 
 //Helper Function to Set Attributes on DOM Elements
 const setAttributes = (element, attributes) =>{
@@ -12,6 +31,8 @@ const setAttributes = (element, attributes) =>{
 
 // Create Elements for links & photos, Add to DOM
 const displayPhotos = () => {
+    imagesLoaded = 0
+    totalImages = photosArray.length
     // Run function for each object in photosArray
     photosArray.forEach((photo) => {
         //Create <a> to link to Unsplash
@@ -28,6 +49,8 @@ const displayPhotos = () => {
             alt: photo.alt_description,
             title: photo.alt_description
         })
+        //Event Listner, check when each is finished loading
+        img.addEventListener('load', imageLoaded)
 
         //Put <img> inside <a>, then put both inside imageContainer Element
         item.appendChild(img)
@@ -35,11 +58,6 @@ const displayPhotos = () => {
 
     })
 }
-
-// Unsplash API
-const count = 100
-const apiKey = 'FKqHOTgeujR0boBSA4NHJTez2AlbMP8d36AF2-71rCw'
-const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
 
 //Get photos from Unsplash API
 async function getPhotos() {
@@ -55,3 +73,11 @@ async function getPhotos() {
 
 //On Load
 getPhotos()
+
+// Check to see if scrolling near bottom of page, Load More Photos
+window.addEventListener('scroll', () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+        ready = false
+        getPhotos()
+    }
+})
