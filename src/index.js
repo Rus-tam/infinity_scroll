2aad8349b3eb9ea0a3bcc032f1d1a7c99c7d3576
Rustam <ahmetov.rustam2011@gmail.com>
2020-12-11 18:19:33 +0500

    displaing_data_1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,43 @@
+const imageContainer = document.getElementById('image-container')
+const loader = document.getElementById('loader')
+
+let photosArray = []
+
+//Helper Function to Set Attributes on DOM Elements
+const setAttributes = (element, attributes) =>{
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key])
+    }
+}
+
+// Create Elements for links & photos, Add to DOM
+const displayPhotos = () => {
+    // Run function for each object in photosArray
+    photosArray.forEach((photo) => {
+        //Create <a> to link to Unsplash
+        const item = document.createElement('a')
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank'
+        })
+
+        //Create <img> for photo
+        const img = document.createElement('img')
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description
+        })
+
+        //Put <img> inside <a>, then put both inside imageContainer Element
+        item.appendChild(img)
+        imageContainer.appendChild(item)
+
+    })
+}
+
 // Unsplash API
-const count = 10
+const count = 100
 const apiKey = 'FKqHOTgeujR0boBSA4NHJTez2AlbMP8d36AF2-71rCw'
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
 
@@ -7,8 +45,9 @@ const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&coun
 async function getPhotos() {
     try {
         const response = await fetch(apiUrl)
-        const data = await response.json()
-        console.log(data)
+        photosArray = await response.json()
+        displayPhotos()
+
     } catch (error) {
         //catch error here
     }
